f2ee84e3500c685aca0712ab6c625df1d4ee3212
henriquesbezerra <henriquesb92@gmail.com>
2020-02-10 13:33:23 -0300

    language/Rust - Hello World, with cargo!

---
+++ Rust/cargo_hello_world/src/main.rs
@@ -0,0 +1,4 @@
+fn main() {
+    let name = "Henrique Silva";
+    println!("Hello, {}", name)
+}
---
+++ Rust/cargo_hello_world/target/debug/cargo_hello_world.d
@@ -0,0 +1,1 @@
+/home/henri/codex/Rust/cargo_hello_world/target/debug/cargo_hello_world: /home/henri/codex/Rust/cargo_hello_world/src/main.rs
