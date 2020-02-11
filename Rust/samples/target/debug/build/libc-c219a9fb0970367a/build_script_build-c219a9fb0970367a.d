99fd348aaab4bf312def12bae2c11efee0873be0
henriquesbezerra <henriquesb92@gmail.com>
2020-02-11 09:27:33 -0300

    fixing

--- Rust/samples/target/debug/build/libc-c219a9fb0970367a/build_script_build-c219a9fb0970367a.d
+++
@@ -1,5 +0,0 @@
-/home/henri/codex/Rust/samples/target/debug/build/libc-c219a9fb0970367a/build_script_build-c219a9fb0970367a: /home/henri/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.66/build.rs
-
-/home/henri/codex/Rust/samples/target/debug/build/libc-c219a9fb0970367a/build_script_build-c219a9fb0970367a.d: /home/henri/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.66/build.rs
-
-/home/henri/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.66/build.rs:
--- Rust/samples/target/debug/samples.d
+++
@@ -1,1 +0,0 @@
-/home/henri/codex/Rust/samples/target/debug/samples: /home/henri/codex/Rust/samples/src/main.rs /home/henri/codex/Rust/samples/src/mutable_variables.rs
