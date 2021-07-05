2c127b1c388e33379535d9a587e781dc83efb70b
henriquesbezerra <henriquesb92@gmail.com>
2021-07-05 20:43:41 -0300

    Resuming studies in c

---
+++ C/basics/hello-world.c
@@ -0,0 +1,14 @@
+// Import de bibliteca padrão de entrada e saida.
+#include <stdio.h>
+
+// função principal de um programa C
+int main()
+{
+  printf("Hello World!");
+}
+
+/***
+ * Compilamos esse programa com
+ * gcc hello-world.c -o hello-worl.exe
+*/ 
+ 
