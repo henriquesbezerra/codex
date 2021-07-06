802769fcd9b07c5e1ce43037bbcc3fb4b131dd78
henriquesbezerra <henriquesb92@gmail.com>
2021-07-05 23:03:57 -0300

    Desafio da multiplicação, recebe dois valores em variaveis e multiplique

---
+++ C/basics/multiplication.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+  int x = 0, y =0;
+
+  printf("\n## Bem vindo a multiplicação de X por Y ##\n");
+
+  printf("\nEntre com x: ");
+  scanf("%d",&x);
+
+
+  printf("\nEntre com y: ");
+  scanf("%d",&y);
+
+  printf("\nO resultado de X*Y é: %d \n\n", x*y );
+
+  return 0;
+}
